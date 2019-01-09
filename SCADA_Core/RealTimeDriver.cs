a7cdf5d4430a298bf1a0731e5679d4afde137201
Boris Sulicenko <b.sulicenko@gmail.com>
2019-01-09 18:41:56 +0100

    Added few more classes and AlarmDiplay, RTU, Trending projects

---
+++ SCADA_Core/RealTimeDriver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCADA_Core
+{
+    public class RealTimeDriver
+    {
+    }
+}
---
+++ SCADA_Core/SimulationDriver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCADA_Core
+{
+    public class SimulationDriver
+    {
+        private const double amplitude = 100;
+
+        public SimulationDriver()
+        {
+        }
+
+        public static double Sine()
+        {
+            return amplitude * Math.Sin((double)DateTime.Now.Second / 60 * Math.PI);
+        }
+
+        public static double Cosine()
+        {
+            return amplitude * Math.Cos((double)DateTime.Now.Second / 60 * Math.PI);
+        }
+
+        public static double Ramp()
+        {
+            return amplitude * DateTime.Now.Second / 60;
+        }
+
+        public static double Triangle()
+        {
+            // @TODO: implement this
+            return 0;
+        }
+
+        public static double Rectangle()
+        {
+            // @TODO: implement this
+            return 0;
+        }
+    }
+}
