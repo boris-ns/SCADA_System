1d11ec595d83de265ef6a0ff769cd3f4dbc64ea8
Boris Sulicenko <b.sulicenko@gmail.com>
2019-01-12 12:47:42 +0100

    Methods for enabling/disabling GUI components when tag type changes.

--- DatabaseManager/DataForm.cs
+++ DatabaseManager/DataForm.cs
@@ -32,15 +32,58 @@ namespace DatabaseManager
 
         }
 
+        private void EnableComponentsForDigitalInput()
+        {
+            btnAddAlarm.Enabled = true;
+            btnRemoveAlarm.Enabled = true;
+            listBoxAlarms.Enabled = true;
+            textBoxScanTime.Enabled = true;
+            textBoxInitValue.Enabled = false;
+            textBoxLowLimit.Enabled = false;
+            textBoxHighLimit.Enabled = false;
+            textBoxUnits.Enabled = false;
+            checkBoxOnOffScan.Enabled = true;
+            checkBoxAutoManual.Enabled = true;
+        }
+
         private void EnableComponentsForDigitalOutput()
         {
             btnAddAlarm.Enabled = false;
             btnRemoveAlarm.Enabled = false;
             listBoxAlarms.Enabled = false;
             textBoxScanTime.Enabled = false;
+            textBoxInitValue.Enabled = true;
             textBoxLowLimit.Enabled = false;
-            labelHighLimit.Enabled = false;
+            textBoxHighLimit.Enabled = false;
             textBoxUnits.Enabled = false;
+            checkBoxOnOffScan.Enabled = false;
+            checkBoxAutoManual.Enabled = false;
+        }
+
+        private void EnableComponentsForAnalogInput()
+        {
+            btnAddAlarm.Enabled = true;
+            btnRemoveAlarm.Enabled = true;
+            listBoxAlarms.Enabled = true;
+            textBoxScanTime.Enabled = true;
+            textBoxInitValue.Enabled = false;
+            textBoxLowLimit.Enabled = true;
+            textBoxHighLimit.Enabled = true;
+            textBoxUnits.Enabled = true;
+            checkBoxOnOffScan.Enabled = true;
+            checkBoxAutoManual.Enabled = true;
+        }
+
+        private void EnableComponentsForAnalogOutput()
+        {
+            btnAddAlarm.Enabled = false;
+            btnRemoveAlarm.Enabled = false;
+            listBoxAlarms.Enabled = false;
+            textBoxScanTime.Enabled = false;
+            textBoxInitValue.Enabled = true;
+            textBoxLowLimit.Enabled = true;
+            textBoxHighLimit.Enabled = true;
+            textBoxUnits.Enabled = true;
             checkBoxOnOffScan.Enabled = false;
             checkBoxAutoManual.Enabled = false;
         }
@@ -49,20 +92,20 @@ namespace DatabaseManager
         {
             switch (comboBoxTagType.SelectedIndex)
             {
-                case 0:
-
+                case 0: // Digital Input
+                    EnableComponentsForDigitalInput();
                     break;
 
-                case 1:
+                case 1: // Digital Output
                     EnableComponentsForDigitalOutput();
                     break;
 
-                case 2:
-
+                case 2: // Analog Input
+                    EnableComponentsForAnalogInput();
                     break;
 
-                case 3:
-
+                case 3: // Analog Output
+                    EnableComponentsForAnalogOutput();
                     break;
 
                 default:
